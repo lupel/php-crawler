2ac7ea3f9cc294d58c7c374abcea51ab1ba25d13
Luciano Pelizari <lupel31@gmail.com>
2013-11-18 12:46:52 -0200

    depth bug fixed.

--- crawler.php
+++ crawler.php
@@ -1,10 +1,10 @@
 <?php
 class crawler{
-    private $_url      = 'www.google.com';
-    private $_depth    = 1;
-    private $_maxLinks = 100;
-    private $_links    = array();
-    private $_started  = 0;
+    private $_url       = 'www.google.com';
+    private $_depth     = 1;
+    private $_maxLinks  = 100;
+    private $_links     = array();
+    private $_started   = 0;
 
     public function setUrl( $url )
     {
@@ -16,47 +16,51 @@ class crawler{
 
     public function setDepth( $depth )
     {
-        if( is_int( $depth ) )
+        if( is_numeric( $depth ) )
         {
             $this->_depth = $depth;
         }
     }
 
-    private function getContent()
+    private function getContent( $url=null )
     {
-		$curl = curl_init();
-		curl_setopt( $curl, CURLOPT_URL, $this->_url );
-		curl_setopt( $curl, CURLOPT_HEADER, false );
-		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-		curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
+        $url = empty( $url ) ? $this->_url : $url;
+
+        $curl = curl_init();
+        curl_setopt( $curl, CURLOPT_URL, $url );
+        curl_setopt( $curl, CURLOPT_HEADER, false );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
         curl_setopt( $curl, CURLOPT_ENCODING , 'UTF-8' );
         curl_setopt( $curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 GTB5' );
-		$data = curl_exec( $curl );
-		curl_close( $curl );
+        $data = curl_exec( $curl );
+        curl_close( $curl );
 
         return $data;
     }
  
-    public function getLinks()
+    public function getLinks( $url=null )
     {
+        $url = empty( $url ) ? $this->_url : $url;
+
         if( !$this->_started )
         {
             $this->_started = 1;
-            $curr_depth = 0;
+            $currDepth = 0;
         }else{
-            $curr_depth++;
+            $currDepth++;
         }
-        if( $curr_depth < $this->_depth )
+        if( $currDepth < $this->_depth )
         {
-            $data = $this->getContent( $this->_url );
-            if( preg_match_all( '/<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>/siU', $data, $urls12 ) )
+            $data = $this->getContent( $url );
+            if( preg_match_all( '/<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>/siU', $data, $listUrls ) )
             {
-                foreach( $urls12[ 2 ] as $index => $value )
+                foreach( $listUrls[ 2 ] as $index => $value )
                 {
                     $parsedUrl = parse_url( $value );
-                    $parsedUrl = $parsedUrl[ 'host' ];
+                    $parsedUrl = isset( $parsedUrl[ 'host' ] ) ? $parsedUrl[ 'host' ] : null;
 
-                    if( !empty( $parsedUrl ) && !stristr( $parsedUrl, $this->_url ) && !in_array( $parsedUrl, $this->_links ) && $curr_depth < $this->_depth && count( $this->_links ) < $this->_maxLinks )
+                    if( !empty( $parsedUrl ) && !stristr( $parsedUrl, $this->_url ) && !in_array( $parsedUrl, $this->_links ) && $currDepth < $this->_depth && count( $this->_links ) < $this->_maxLinks )
                     {
                         $this->_links[] = $parsedUrl;
                         $this->getLinks( $parsedUrl );
@@ -74,10 +78,9 @@ class crawler{
     public function toJson()
     {
         header( 'Content-Type: application/json' );
-        echo( json_encode( $this->_links ) );
+        echo( json_encode( array( 'totalFound' => count( $this->_links ), 'links' => $this->_links ) ) );
     }
 }
-
 
 set_time_limit( 0 );
 
